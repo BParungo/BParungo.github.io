5a836ede041cbea64d38e03999501f40d59db1c7
bparu <bparungo@web.de>
2023-05-28 18:27:13 +0200

    Add some content and expiremnting with styles.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+const cursorX = ref('0px');
+const cursorY = ref('0px')
+function updateCoordinates(event:MouseEvent) {
+      cursorX.value = event.clientX+'px';
+      cursorY.value = event.clientY+'px';
+      }
+
+onMounted(()=>{
+  document.addEventListener("mousemove", (event) => {
+    updateCoordinates(event)
+				});
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+  <div id="appcontent">
+    <div class="wrapper" >
+      <p>{{ [cursorX,cursorY] }}</p>
       <HelloWorld msg="You did it!" />
 
       <nav>
@@ -15,71 +27,30 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
-
   <RouterView />
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style scoped >
+.wrapper{
+  max-width: 1128px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#appcontent{
+  height: 100%;
+  position: relative;
+  overflow: hidden;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#appcontent::before{
+  content: "";
+  background: radial-gradient(at right center, #4CCEEB, #6777A2);;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  left: v-bind('cursorX');
+  top: v-bind('cursorY');
+  transform: translate(-50%,-50%);
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,35 +6,18 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
+    <h1>Can-Blaine Parungo</h1>
+    <h2>Frontend Entwickler</h2>
+    <h3>UX/UI Designer
+      Frontend Entwickler & UX/UI - Designer
+      <h4>bei der Deutschen Gesellschaft für Cybersicherheit</h4>
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
+    <p>Als Frontend-Entwickler und UX/UI-Designer erzeuge ich faszinierende Nutzererlebnisse, indem ich ästhetisches Design und technische Umsetzung miteinander vereine.</p>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
