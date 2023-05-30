2ce04aa9893549a58ffd37a27cf22f1d417e7e00
BParungo <BParungo@users.noreply.github.com>
2023-05-30 10:44:00 +0000

    add Build

---
+++ dist/assets/ProjectsView-aa5a41ff.js
@@ -0,0 +1,1 @@
+import{_ as o,o as c,c as s,p as t,b as _,a}from"./index-4489c0c2.js";const n={},d=e=>(t("data-v-6430f150"),e=e(),_(),e),r=d(()=>a("h1",null,"Projects View",-1)),p=[r];function i(e,l){return c(),s("div",null,p)}const u=o(n,[["render",i],["__scopeId","data-v-6430f150"]]);export{u as default};
