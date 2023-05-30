f6066fb64f1214cc814618c73367a6354a3ddcb9
BlaineParungo <blaine.parungo@dgc.org>
2023-05-30 11:55:05 +0200

    test actions

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ onMounted(()=>{
   <div id="app-content">
     <div class="l-wrapper" >
       <IntroductionBlock :title="me.title" :work-at="me.workAt" :quote="me.quote"/>
-      testiasdaasaang
+      testiasdaasaaang
       <SideNavigation></SideNavigation>
     </div>
     <div class="r-wrapper">
