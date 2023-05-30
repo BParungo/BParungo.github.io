071ccbd8826e4936282ba937358b0679b41e1006
BParungo <BParungo@users.noreply.github.com>
2023-05-30 10:29:50 +0000

    add Build

---
+++ dist/assets/ContactView-3e14f7da.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c,a as n}from"./index-cb0d1a2e.js";const o={},_=n("h1",null,"Contact View",-1),s=[_];function a(r,i){return t(),c("div",null,s)}const d=e(o,[["render",a]]);export{d as default};
