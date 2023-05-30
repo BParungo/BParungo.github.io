91a46f305d7552257ed107d7073b302d0b7008c6
BParungo <BParungo@users.noreply.github.com>
2023-05-30 10:46:52 +0000

    add Build

---
+++ dist/assets/AboutView-a5964944.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as _}from"./index-9142e898.js";const s={},n=_("p",null,"This is an about page",-1),c=[n];function a(r,i){return t(),o("div",null,c)}const u=e(s,[["render",a]]);export{u as default};
