9e47c94141eaefe4d09e6fe3b19269653a978767
BlaineParungo <blaine.parungo@dgc.org>
2023-05-30 13:14:56 +0200

    test

--- dist/assets/ContactView-3e67173c.js
+++ dist/assets/ContactView-3e67173c.js
@@ -1,1 +1,5 @@
+<<<<<<<< HEAD:dist/assets/ContactView-3e67173c.js
 import{_ as e,o as t,c,a as n}from"./index-cfb3e560.js";const o={},_=n("h1",null,"Contact View",-1),s=[_];function a(r,i){return t(),c("div",null,s)}const d=e(o,[["render",a]]);export{d as default};
+========
+import{_ as e,o as t,c,a as n}from"./index-d4b01087.js";const o={},_=n("h1",null,"Contact View",-1),s=[_];function a(r,i){return t(),c("div",null,s)}const d=e(o,[["render",a]]);export{d as default};
+>>>>>>>> d8da1e2 (test):dist/assets/ContactView-37625b55.js
--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ onMounted(()=>{
   <div id="app-content">
     <div class="l-wrapper" >
       <IntroductionBlock :title="me.title" :work-at="me.workAt" :quote="me.quote"/>
-      testiasdaasdasdasasaaaaaaaaaaaaang
+      testiasdaasdasdasasaaadaaaaaaaaaang
       <SideNavigation></SideNavigation>
     </div>
     <div class="r-wrapper">
