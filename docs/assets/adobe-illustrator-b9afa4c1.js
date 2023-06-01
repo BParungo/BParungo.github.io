dcf4e7ba5f651e07dec92a959a2f56b15ada9d27
BParungo <BParungo@users.noreply.github.com>
2023-06-01 12:27:54 +0000

    add Build

--- docs/assets/adobe-illustrator-b9afa4c1.js
+++
@@ -1,1 +0,0 @@
-const t=""+new URL("adobe-illustrator-4e48c216.svg",import.meta.url).href;export{t as default};
--- docs/assets/adobe-photoshop-35191651.js
+++
@@ -1,1 +0,0 @@
-const o=""+new URL("adobe-photoshop-0fd9e8a3.svg",import.meta.url).href;export{o as default};
