dab86efd453336e9192f7238ec362f21e3821deb
bparungo <bparungo@web.de>
2025-02-04 10:03:45 +0100

    update packages

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,17 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '',
+  plugins: [vue(), vueDevTools()],
   build: {
-    outDir: './docs'
+    outDir: './docs',
   },
-  publicDir: "public",
+  publicDir: 'public',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  }
+    },
+  },
 })
