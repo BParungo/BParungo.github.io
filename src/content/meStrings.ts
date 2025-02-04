e68dccfd12d992032df2341f97c9a5e87fe9228b
bparungo <bparungo@web.de>
2025-02-04 11:07:06 +0100

    update current work

--- src/content/meStrings.ts
+++ src/content/meStrings.ts
@@ -1,6 +1,6 @@
 export const me = {
   title: 'Frontend-Entwickler \n & UX/UI-Designer',
-  workAt: 'bei der Deutsche Gesellschaft für Cybersicherheit',
+  workAt: 'bei mwb fairtrade Wertpapierhandelsbank AG',
   hello: 'Moin,',
   introduction:
     'ich bin Blaine Parungo, Frontend-Entwickler und UX/UI-Designer. Meine Leidenschaft gilt der Erstellung ansprechender Benutzeroberflächen und der Gestaltung nahtloser Benutzererfahrungen',
@@ -8,5 +8,5 @@ export const me = {
     'Als Frontend-Entwickler arbeite ich daran, Designs in lebendige, interaktive Webanwendungen umzusetzen. Dabei arbeite ich eng mit Designern, Back-End-Entwicklern und anderen Teammitgliedern zusammen, um innovative Lösungen zu entwickeln, die den Bedürfnissen der Benutzer gerecht werden.',
   uxText:
     'Als UX/UI-Designer gestalte ich intuitive und benutzerfreundliche Schnittstellen. Mein Ziel ist es, die Bedürfnisse der Benutzer zu verstehen und ihre Erfahrungen durch maßgeschneiderte Designs zu verbessern.',
-  text: 'Mit meiner technischen Expertise und meiner kreativen Denkweise strebe ich danach, kontinuierlich Mehrwert zu schaffen und innovative Lösungen zu entwickeln. Folgende Fähigkeiten und Tools habe ich bereits kennenlernen dürfen:'
+  text: 'Mit meiner technischen Expertise und meiner kreativen Denkweise strebe ich danach, kontinuierlich Mehrwert zu schaffen und innovative Lösungen zu entwickeln. Folgende Fähigkeiten und Tools habe ich bereits kennenlernen dürfen:',
 }
